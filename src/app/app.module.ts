bac478533c38ab76f3d7fbc1aca742e02581abba
ankit-pandey-23 <ankit.pandey@tatvasoft.com>
2020-03-20 09:44:43 +0530

    listing and dialog box structure implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatInputModule,MatButtonModule,MatDialogModule,MatTableModule} from '@angular/material'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { DialogComponent } from './components/dialog/dialog.component';
+import { PollsListingComponent } from './components/polls-listing/polls-listing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogComponent,
+    PollsListingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatTableModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      { path: '', component: PollsListingComponent },
+      { path: 'list',component: PollsListingComponent },
+    ])
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [DialogComponent]
 })
 export class AppModule { }
